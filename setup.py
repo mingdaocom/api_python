29e58b2bf7419fe3c802d2dc3e84bd4d6557b2cc
Sin <dev@zouxin.net>
2014-04-27 09:21:39 +0800

    添加数据文件

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 import distutils.core
 
-version = '0.1.2'
+version = '0.1.3'
 
 distutils.core.setup(
 	name='mingdao',
 	version=version,
+    keywords = ('mingdao', 'oauth2'),
 	packages=['mingdao'],
+    package_dir={'mingdao':'mingdao'},
+    package_data={'mingdao':['data/*.json']},
 	author='Sin',
 	author_email='sin.zou@mindao.com',
 	url='http://github.com/ipy/api_python',
 	description='Python SDK for mingdao.com, provides Mingdao OAuth2 authorization and API wrapper.',
+    long_description = open('README.md').read(),
 	install_requires=[
 		'requests',
 	],
